ed91a8fadc067fad7e5e0f45dff3d00ae4ee9017
dario de stefano <dario.destefano@gmail.com>
2023-10-18 15:14:49 +0100

    bug fixes and refactored the function as per instructions

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -6,24 +6,6 @@ word_list=['banana','apple','blueberries','raspberries','peach']
 word=random.choice(word_list)
 #print(word)
 
-guess=input("please insert a letter:")
-
-
-def ask_for_input():
-    """ Ask for user's input """
-
-    while True: 
-
-        if len(guess)==1 and guess.isalpha():
-
-            print("good guess!")
-            break
-
-        else: 
-
-            print("Invalid letter. Please, enter a single alphabetical character.")
-
-
 
 def check_guess(guess,word):
 
@@ -44,3 +26,26 @@ def check_guess(guess,word):
     else: 
 
         print(f"Sorry, {guess} is not in the word. Try again.")
+
+
+
+def ask_for_input(word):
+    """ Ask for user's input """
+
+    while True: 
+        
+        guess=input("please insert a letter:") 
+
+        if len(guess)==1 and guess.isalpha():
+
+            print("guess is in the right format")
+            break
+
+        else: 
+
+            print("Invalid letter. Please, enter a single alphabetical character.")
+
+    check_guess(guess,word)
+
+ask_for_input(word)
+
