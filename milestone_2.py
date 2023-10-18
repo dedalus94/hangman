b833da4898eea8a75933a119ca2a828e7320233c
dario de stefano <dario.destefano@gmail.com>
2023-10-18 14:28:25 +0100

    adds checks for acceptable variables

--- milestone_2.py
+++ milestone_2.py
@@ -1,2 +1,17 @@
+import random 
+
 word_list=['banana','apple','blueberries','raspberries','peach']
 print(word_list)
+
+word=random.choice(word_list)
+print(word)
+
+guess=input("please insert a letter:")
+
+if len(guess)==1 and guess.isalpha():
+
+    print("good guess!")
+
+else: 
+
+    print("Oops! That is not a valid input.")
